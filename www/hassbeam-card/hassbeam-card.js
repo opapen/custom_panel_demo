8e48cc3106f9992786dfc2a0567e6002742855dd
ovonpape <ole.von_papen@smail.th-koeln.de>
2025-07-08 22:01:02 +0200

    2

--- www/hassbeam-card/hassbeam-card.js
+++
@@ -1,9 +0,0 @@
-class HassBeamCard extends HTMLElement {
-  setConfig(config) {
-    this.innerHTML = `<div style="padding:1em; border:1px solid #ccc;">
-        <h3>${config.title || "HassBeam Card"}</h3>
-        <p>Status: OK</p>
-      </div>`;
-  }
-}
-customElements.define('hassbeam-card', HassBeamCard);
