6cfccd119632ceaae280c9c345329f8af5ac7687
ovonpape <ole.von_papen@smail.th-koeln.de>
2025-07-09 15:14:30 +0200

    sdfsdf

--- custom_components/custom_panel_demo/__init__.py
+++ custom_components/custom_panel_demo/__init__.py
@@ -1,8 +1,13 @@
 from homeassistant.components.frontend import async_register_built_in_panel
+import logging
+_LOGGER = logging.getLogger(__name__)
+_LOGGER.info("Setting up Custom Panel Demo...")
+
 
 DOMAIN = "custom_panel_demo"
 
 async def async_setup(hass, config):
+    _LOGGER.info("registering path for custom panel demo")
     hass.http.register_static_path(
         "/custom_panel_demo_panel",
         hass.config.path("www/custom_panel_demo_panel"),
@@ -11,7 +16,7 @@ async def async_setup(hass, config):
 
     async_register_built_in_panel(
         hass,
-        component_name="iframe",  # Alternative: "custom" für echte WebComponent
+        component_name="iframe",
         sidebar_title="Custom Panel",
         sidebar_icon="mdi:tools",
         frontend_url_path="custom-panel",
